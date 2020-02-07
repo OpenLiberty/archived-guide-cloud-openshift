284d9b8a12de90c3ea46308aec53c875d4461386
Mai Hameed <maihameed@hotmail.com>
2020-02-07 11:00:35 -0500

    Matched start to finish

--- start/inventory/src/main/java/io/openliberty/guides/inventory/client/SystemClient.java
+++ start/inventory/src/main/java/io/openliberty/guides/inventory/client/SystemClient.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2017 IBM Corporation and others.
+ * Copyright (c) 2017, 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -21,45 +21,23 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import java.util.Properties;
-import java.net.URI;
 
 @RequestScoped
 public class SystemClient {
 
   // Constants for building URI to the system service.
-  private final int DEFAULT_PORT = Integer.valueOf(System.getProperty("default.http.port"));
   private final String SYSTEM_PROPERTIES = "/system/properties";
-  private final String PROTOCOL = "http";
+  private final String PROTOCOL = "http://";
 
   // Wrapper function that gets properties
   public Properties getProperties(String hostname) {
-    String url = buildUrl(PROTOCOL, hostname, DEFAULT_PORT, SYSTEM_PROPERTIES);
+    String url = buildUrl(PROTOCOL, hostname, SYSTEM_PROPERTIES);
     Builder clientBuilder = buildClientBuilder(url);
     return getPropertiesHelper(clientBuilder);
   }
 
-  // tag::doc[]
-  /**
-   * Builds the URI string to the system service for a particular host.
-   * @param protocol
-   *          - http or https.
-   * @param host
-   *          - name of host.
-   * @param port
-   *          - port number.
-   * @param path
-   *          - Note that the path needs to start with a slash!!!
-   * @return String representation of the URI to the system properties service.
-   */
-  // end::doc[]
-  protected String buildUrl(String protocol, String host, int port, String path) {
-    try {
-      URI uri = new URI(protocol, null, host, port, path, null, null);
-      return uri.toString();
-    } catch (Exception e) {
-      System.err.println("Exception thrown while building the URL: " + e.getMessage());
-      return null;
-    }
+  protected String buildUrl(String protocol, String host, String path) {
+    return protocol+host+path;
   }
 
   // Method that creates the client builder
