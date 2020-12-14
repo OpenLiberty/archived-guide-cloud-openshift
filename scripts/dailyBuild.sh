ee83a1cf732e5174b64882136380fea756a25aff
Tien Thanh Le <44335153+tt-le@users.noreply.github.com>
2020-12-14 15:17:47 -0500

    Update dailyBuild.sh

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -17,3 +17,39 @@ cat inventory/Dockerfile system/Dockerfile
 docker pull $DOCKER_USERNAME"/olguides:"$BUILD
 
 sudo ../scripts/testApp.sh
+
+#!/bin/bash
+while getopts t:d:b:u: flag; do
+    case "${flag}" in
+    t) DATE="${OPTARG}" ;;
+    d) DRIVER="${OPTARG}" ;;
+    b) BUILD="${OPTARG}" ;;
+    u) DOCKER_USERNAME="${OPTARG}" ;;
+    esac
+done
+
+echo "Testing daily build image"
+
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install></configuration>" inventory/pom.xml system/pom.xml
+cat inventory/pom.xml system/pom.xml
+
+sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM "$DOCKER_USERNAME"/olguides:"$BUILD";g" inventory/Dockerfile system/Dockerfile
+cat inventory/Dockerfile system/Dockerfile
+
+sudo ../scripts/testApp.sh
+
+kubectl delete -f ./test.yaml
+eval $(minikube docker-env -u)
+minikube stop
+minikube delete
+
+
+echo "Testing daily Docker image"
+
+sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM openliberty/daily:latest;g" system/Dockerfile inventory/Dockerfile
+
+cat system/Dockerfile inventory/Dockerfile
+
+docker pull "openliberty/daily:latest"
+
+../scripts/testApp.sh
